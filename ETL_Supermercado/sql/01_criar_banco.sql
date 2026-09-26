2046222314412258e8ec36ba1396789e73045094
Sérgio R. Reeck Filho <s_reeck@hotmail.com>
2026-09-25 21:11:55 -0300

    Outras alterações

--- ETL_Supermercado/sql/01_criar_banco.sql
+++ ETL_Supermercado/sql/01_criar_banco.sql
@@ -1,13 +1,15 @@
+-- Active: 1790377505119@@127.0.0.1@5432@etl_vendas_supermercado
 -- ============================================================
--- CRIAÇÃO DO BANCO DE DADOS DO PROJETO
--- ============================================================
-
-CREATE DATABASE vendas_filiais_supermercado;
-
--- ============================================================
--- LIMPEZA E CRIAÇÃO DO(S) SCHEMA(S)
+-- Criação do banco de dados do projeto ETL
 -- ============================================================
 
+CREATE DATABASE etl_vendas_supermercado;
+
+-- ============================================================
+-- Limpeza do schema public e criação do schema vendas
+-- ============================================================
+
+DROP SCHEMA IF EXISTS public CASCADE;
 DROP SCHEMA IF EXISTS vendas CASCADE;
 
 CREATE SCHEMA vendas;
